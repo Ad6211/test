ce885c8cddb61b24f0e13d994145588168c35317
ad6211 <adamjoliclerc@gmail.com>
2026-03-01 18:30:17 +0100

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById('btn').addEventListener('click', function() {
-    alert('Coucou ! Ton site marche ! 🎉');
+    alert('Coucou ! Ton site marche ! 🎉 (ps c juste un p*n de test)');
+});
+document.getElementById('btn2').addEventListener('click', function() {
+    alert('Tu as cliqué sur le deuxième bouton ! tu as vraiment que ça à foutre ? 😂 ');
 });
